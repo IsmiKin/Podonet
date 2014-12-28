ab6e926a66adf2a3503bb50fc825ad9b84556bbd
ismikin <ismikin@gmail.com>
2014-12-28 12:40:25 +0100

    Peleando con Edit

--- web/js/agenda/ajustes_agenda.js
+++ web/js/agenda/ajustes_agenda.js
@@ -1,26 +1,56 @@
 /**
- * Created by root on 23/12/14.
+ * Created by IsmiKin on 23/12/14.
  */
+
+// Version "semi-automatica"
+var camposEnColumnas = ["codigo","localizacion","tipo"];
+var camposEnFilas = ["estado"];
+var rowEditando ;
 
 $(document).ready(function(){
 
     $(".form-gabinete").submit(submitFormGabinete); // Para la creacion
-    $(".submitEditarGabinete").click(submitEditarFormGabinete);
+    $(".form-editar-gabinete").submit(submitEditarFormGabinete)
+    //$(".submitEditarGabinete").click(submitEditarFormGabinete);
     $(".habilitarGabineteButton").click(habilitarGabinete);
+    $(".crearModalButton").click(showCreateModal);
+    $(".editarGabineteTabla").click(showEditModal);
+
+    // Version alternativa
+
 });
 
 function submitFormGabinete(e){
     e.preventDefault();
+    var form = $(".form-gabinete");
+    var botonCrear = $(".submitCrearGabinete");
+    var botonEditar = $(".submitEditarGabinete");
+    var idGabinete = null;
+    var opcion = null;
+
+    if(rowEditando!=undefined)
+        idGabinete = rowEditando.data("idgabinete");
+
+    var values = {};
+    $.each( form.serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+    console.log(form.serializeArray());
+
+    if(botonCrear.is(":visible")){
+        opcion = botonCrear;
+    }else{
+        opcion = botonEditar;
+    }
+
     $.ajax({
-        type: $(this).attr('method'),
-        url: $(this).attr('action'),
-        data: $(this).serialize(),
+        type: opcion.data("method"), url: opcion.data("action"),
+        data: values,
         success: function(data) {
             if(data.codigo_error==0){
                 var gabinete = $.parseJSON(data.gabinete);
-                var htmlNuevaFila = Handlebars.templates.nuevafila_gabinete(gabinete);
-                $(".table-gabinetes tr:last").after(htmlNuevaFila);
-                $(".habilitarGabineteButton").click(habilitarGabinete);
+                insertarNuevaRow(gabinete);
+
                 resetearForm();
                 $(".cancelarGabineteForm").click();
             }else
@@ -31,8 +61,71 @@ function submitFormGabinete(e){
 
 }
 
+function submitEditarFormGabinete(e){
+    e.preventDefault();
+    console.log("yija");
+    var form = $(".form-editar-gabinete");
+    var botonCrear = $(".submitCrearGabinete");
+    var botonEditar = $(".submitEditarGabinete");
+    var idGabinete = null;
+    var opcion = null;
+
+    if(rowEditando!=undefined)
+        idGabinete = rowEditando.data("idgabinete");
+
+    var values = {};
+    $.each( form.serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+    console.log(form.serializeArray());
+
+    if(botonCrear.is(":visible")){
+        opcion = botonCrear;
+    }else{
+        opcion = botonEditar;
+        values["idGabinete"] = idGabinete;
+    }
+
+    $.ajax({
+        type: opcion.data("method"), url: opcion.data("action"),
+        data: values,
+        success: function(data) {
+            if(data.codigo_error==0){
+                var gabinete = $.parseJSON(data.gabinete);
+                insertarNuevaRow(gabinete);
+
+                resetearForm();
+                $(".cancelarGabineteForm").click();
+            }else
+                console.log("error!");
+
+        }
+    });
+
+    console.log("webe");
+}
+
+
 function resetearForm(){
     $(".form-gabinete")[0].reset();
+}
+
+function insertarNuevaRow(gabinete){
+    var htmlNuevaFila = Handlebars.templates.nuevafila_gabinete(gabinete);
+    $(".table-gabinetes tr:last").after(htmlNuevaFila);
+    $(".habilitarGabineteButton").click(habilitarGabinete);
+    $(".editarGabineteTabla").click(showEditModal);
+}
+
+function actualizarRow(gabinete){
+    console.log(gabinete);
+    $.each(camposEnColumnas,function(index,value){
+       row.find(".td"+value).data();
+
+    });
+    $.each(camposEnFilas,function(index,value){
+        formulario.find('[name*='+value+']').val(row.data(value));
+    });
 }
 
 function habilitarGabinete(){
@@ -59,24 +152,24 @@ function habilitarGabinete(){
 
 }
 
-function submitEditarFormGabinete(e){
-    e.preventDefault();
-    $.ajax({
-        type: $(this).data('method'),
-        url: $(this).data('action'),
-        data: $(".form-gabinete").serialize(),
-        success: function(data) {
-            if(data.codigo_error==0){
-                var gabinete = $.parseJSON(data.gabinete);
-                var htmlNuevaFila = Handlebars.templates.nuevafila_gabinete(gabinete);
-                $(".table-gabinetes tr:last").after(htmlNuevaFila);
-                $(".habilitarGabineteButton").click(habilitarGabinete);
-                resetearForm();
-                $(".cancelarGabineteForm").click();
-            }else
-                console.log("error!");
 
-        }
+function showCreateModal(e){
+    resetearForm();
+}
+
+function showEditModal(){
+    resetearForm();
+    rowEditando = $(this).parent().parent();
+    bindDatosForm(rowEditando);
+}
+
+// Este metodo habria que hacerlo "automatico"
+function bindDatosForm(row){
+    var formularioEditar = $(".form-editar-gabinete");
+    $.each(camposEnColumnas,function(index,value){
+        formularioEditar.find('[name*='+value+']').val(row.find(".td"+value).data(value));
     });
-
+    $.each(camposEnFilas,function(index,value){
+        formularioEditar.find('[name*='+value+']').val(row.data(value));
+    });
 }
--- web/templates/agenda/compiled_agenda.js
+++ web/templates/agenda/compiled_agenda.js
@@ -16,20 +16,28 @@ templates['nuevafila_gabinete'] = template({"1":function(depth0,helpers,partials
   buffer += "\n";
   stack1 = ((helpers.ifCond || (depth0 && depth0.ifCond) || helperMissing).call(depth0, (depth0 != null ? depth0.estado : depth0), "==", "Inactivo", {"name":"ifCond","hash":{},"fn":this.program(3, data),"inverse":this.noop,"data":data}));
   if (stack1 != null) { buffer += stack1; }
-  buffer += "\n        data-idGabinete=\""
+  buffer += "\n        data-idgabinete=\""
     + escapeExpression(((helper = (helper = helpers.id_gabinete || (depth0 != null ? depth0.id_gabinete : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id_gabinete","hash":{},"data":data}) : helper)))
-    + "\"\n>\n<td class=\"tdcodigo\">"
+    + "\"\n        data-estado=\""
+    + escapeExpression(((helper = (helper = helpers.estado || (depth0 != null ? depth0.estado : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"estado","hash":{},"data":data}) : helper)))
+    + "\"\n>\n<td class=\"tdcodigo\" data-codigo=\""
     + escapeExpression(((helper = (helper = helpers.codigo || (depth0 != null ? depth0.codigo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"codigo","hash":{},"data":data}) : helper)))
-    + "</td>\n<td class=\"tdlocalizacion\">"
+    + "\">"
+    + escapeExpression(((helper = (helper = helpers.codigo || (depth0 != null ? depth0.codigo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"codigo","hash":{},"data":data}) : helper)))
+    + "</td>\n<td class=\"tdlocalizacion\" data-localizacion=\""
     + escapeExpression(((helper = (helper = helpers.localizacion || (depth0 != null ? depth0.localizacion : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"localizacion","hash":{},"data":data}) : helper)))
-    + "</td>\n<td class=\"tdtipo\">"
+    + "\">"
+    + escapeExpression(((helper = (helper = helpers.localizacion || (depth0 != null ? depth0.localizacion : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"localizacion","hash":{},"data":data}) : helper)))
+    + "</td>\n<td class=\"tdtipo\" data-tipo=\""
     + escapeExpression(((helper = (helper = helpers.tipo || (depth0 != null ? depth0.tipo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"tipo","hash":{},"data":data}) : helper)))
-    + "</td>\n<td class=\"tdacciones\">\n    <button class=\"btn btn-warning editarGabineteTabla\" ><i class=\"fa fa-edit\"></i></button>\n\n                <span ";
+    + "\">"
+    + escapeExpression(((helper = (helper = helpers.tipo || (depth0 != null ? depth0.tipo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"tipo","hash":{},"data":data}) : helper)))
+    + "</td>\n<td class=\"tdacciones\">\n    <button class=\"btn btn-warning editarGabineteTabla\" data-toggle=\"modal\" data-target=\"#modalForm\"><i class=\"fa fa-edit\"></i></button>\n\n    <span ";
   stack1 = ((helpers.ifCond || (depth0 && depth0.ifCond) || helperMissing).call(depth0, (depth0 != null ? depth0.estado : depth0), "==", "Activo", {"name":"ifCond","hash":{},"fn":this.program(5, data),"inverse":this.noop,"data":data}));
   if (stack1 != null) { buffer += stack1; }
-  buffer += ">\n                    <button class=\"btn btn-success habilitarGabineteButton\" data-habilitar=\"true\" ><i class=\"fa fa-check\"></i></button>\n                </span>\n                <span ";
+  buffer += ">\n        <button class=\"btn btn-success habilitarGabineteButton\" data-habilitar=\"true\" ><i class=\"fa fa-check\"></i></button>\n    </span>\n    <span ";
   stack1 = ((helpers.ifCond || (depth0 && depth0.ifCond) || helperMissing).call(depth0, (depth0 != null ? depth0.estado : depth0), "==", "Inactivo", {"name":"ifCond","hash":{},"fn":this.program(5, data),"inverse":this.noop,"data":data}));
   if (stack1 != null) { buffer += stack1; }
-  return buffer + ">\n                    <button class=\"btn btn-danger habilitarGabineteButton\" data-habilitar=\"false\"><i class=\"fa fa-minus-circle\"></i></button>\n                </span>\n\n</td>\n\n</tr>";
+  return buffer + ">\n        <button class=\"btn btn-danger habilitarGabineteButton\" data-habilitar=\"false\"><i class=\"fa fa-minus-circle\"></i></button>\n    </span>\n\n</td>\n\n</tr>";
 },"useData":true});
 })();
