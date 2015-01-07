8965562f336768b084a5b42fadde876fabc36ab2
ismikin <ismikin@gmail.com>
2015-01-07 19:29:46 +0100

    Avance del perfil

--- src/AppBundle/Controller/UsuarioController.php
+++ src/AppBundle/Controller/UsuarioController.php
@@ -3,30 +3,49 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use JMS\Serializer\SerializerBuilder as Serializer;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UsuarioController extends Controller
 {
     public function perfilUsuarioAction()
     {
-        return $this->render('AppBundle:Usuario/perfilUsuario.html.twig', array(
-                // ...
-            ));    }
+        return $this->render('Usuario/perfilUsuario.html.twig');
+    }
 
     public function editarPerfilAction()
     {
-        return $this->render('AppBundle:Usuario/editarPerfil.html.twig', array(
+        return $this->render('Usuario/editarPerfil.html.twig', array(
                 // ...
             ));    }
 
-    public function contactoAdministradorAction()
+    public function contactoAdministradorAction(Request $request)
     {
-        return $this->render('AppBundle:Usuario/contactoAdministrador.html.twig', array(
-                // ...
-            ));    }
+
+        if($request->getMethod()=="POST"){
+
+
+
+
+            $serializer = Serializer::create()->build();
+            //$data = $serializer ->serialize($gabinete, 'json');
+            $mensaje = "hola";
+            $codigo_error ="0";
+
+            $datosRespuesta = array("mensaje" =>$mensaje, "codigo_error" =>$codigo_error);
+            return new JsonResponse($datosRespuesta);
+        }else{
+            return $this->render('Usuario/contactoAdministrador.html.twig', array(
+
+            ));
+        }
+
+    }
 
     public function ayudaUsuarioAction()
     {
-        return $this->render('AppBundle:Usuario/ayudaUsuario.html.twig', array(
+        return $this->render('Usuario/ayudaUsuario.html.twig', array(
                 // ...
             ));    }
 
---
+++ web/js/usuario/perfil/contacto.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ismikin on 6/01/15.
+ */
+
+$(document).ready(function(){
+
+});
+
